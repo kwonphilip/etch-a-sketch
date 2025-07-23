33a8896a14311c03fc780eccada3f78525d70e3b
Philip Kwon <35923674+kwonphilip@users.noreply.github.com>
2025-07-23 15:51:07 -0400

    created separate functions for creating grid rows and squares

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+function createGridRow(row) {
+    const gridRow = document.createElement('div');
+    gridRow.classList.add('grid-row');
+    gridRow.id = `row-${row}`;
+
+    return gridRow;
+}
+
+function createGridSquare(row, col) {
+    const square = document.createElement('div');
+    square.classList.add('grid-square');
+    square.id = `square-row-${row}-col-${col}`;
+
+    return square;
+}
+
 function createGrid(size = 16) {
     const gridContainer = document.querySelector('#grid-container');
 
     for (let row = 0; row < size; row++) {
-        const gridRow = document.createElement('div');
-        gridRow.classList.add('grid-row');
-        gridRow.id = `row-${row}`;
-        
+        const gridRow = createGridRow(row);
+
         for (let col = 0; col < size; col++) {
-            const square = document.createElement('div');
-            square.classList.add('grid-square');
-            square.id = `square-row-${row}-col-${col}`;
+            const square = createGridSquare(row, col);
+
             gridRow.appendChild(square);
         }
 
